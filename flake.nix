f6bab70e57e3970a2c019a276f17bb4c162a93fe
amesgen <amesgen@amesgen.de>
2024-12-30 16:08:02 +0100

    Add GHC 9.12 to CI

--- flake.nix
+++ flake.nix
@@ -5,12 +5,16 @@
     ghc-wasm-meta.url = "gitlab:haskell-wasm/ghc-wasm-meta?host=gitlab.haskell.org";
   };
   outputs = inputs: inputs.flake-utils.lib.eachDefaultSystem (system:
-    let pkgs = inputs.nixpkgs.legacyPackages.${system};
-    in {
-      devShells.default = pkgs.mkShell {
+    let
+      pkgs = inputs.nixpkgs.legacyPackages.${system};
+      inherit (pkgs) lib;
+      devShells = lib.genAttrs [ "9_10" "9_12" ] (ghc: pkgs.mkShell {
         packages = [
-          inputs.ghc-wasm-meta.packages.${system}.all_9_10
+          inputs.ghc-wasm-meta.packages.${system}."all_${ghc}"
         ];
-      };
+      });
+    in
+    {
+      devShells = devShells // { default = devShells."9_10"; };
     });
 }
